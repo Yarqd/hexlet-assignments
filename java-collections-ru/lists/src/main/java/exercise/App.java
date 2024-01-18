0e76eb3b73d7abe6d53f4483fb514fdccd7283d9
@hexlet/cli <support@hexlet.io>
2024-01-18 17:03:57 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -8,22 +8,28 @@ import java.util.List;
 public static boolean scrabble(String set, String word) {
     String lowerSet = set.toLowerCase();
     String lowerWord = word.toLowerCase();
-    if (word > set) {
+    if (word.length() > set.length()) {
         return false;
-        }
-        List<String> listSet = new ArrayList<>();
-    for (var i = 0; i < lowerSet.length; i++) {
-        listSet[i] = lowerSet.charAt(i);
-        }
-    for (var i = 0; i < lowerWord.length; i++) {
-        char  checkChar = lowerWord.charAt(i);
-        for (var j = 0; j < listSet.length; j++) {
-            if (!listSet.contains(checkChar)) {
+    }
+    List<String> listSet = new ArrayList<>();
+    for (var i = 0; i < lowerSet.length(); i++) {
+        String x = String.valueOf(lowerSet.charAt(i));
+        listSet.add(i, x);
+    }
+    for (var i = 0; i < lowerWord.length(); i++) {
+        //char  checkChar = lowerWord.charAt(i);
+        String z = String.valueOf(lowerWord.charAt(i));
+        // z = Взятая буква из слова
+        for (var j = 0; j < listSet.size(); j++) {
+            if (!listSet.contains(z)) {
                 return false;
             } else {
-                listSet.remove(j)
+                listSet.remove(i);
+                break;
+            }
         }
     }
-        return true;
+    return false;
+}
 }
 //END
