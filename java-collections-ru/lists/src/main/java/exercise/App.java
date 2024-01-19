b319713a44e5567564e231d38920a1db63580f84
@hexlet/cli <support@hexlet.io>
2024-01-19 19:25:36 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -5,28 +5,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
-public static boolean scrabble(String set, String word) {
-    String lowerWord = word.toLowerCase();
-    if (word.length() > set.length()) {
-        return false;
-    }
-    List<String> listSet = new ArrayList<>();
-    for (var i = 0; i < set.length(); i++) {
-        String x = String.valueOf(set.charAt(i));
-        listSet.add(i, x);
-    }
-    for (var i = 0; i < lowerWord.length(); i++) {
-        String z = String.valueOf(lowerWord.charAt(i));
-        for (var j = 0; j < listSet.size(); j++) {
-            if (!listSet.contains(z)) {
-                return false;
-            } else {
-                int index = listSet.indexOf(z);
-                listSet.remove(index);
-                break;
+public class scrabble {
+    public static boolean scrabble(String set, String word) {
+        String lowerWord = word.toLowerCase();
+        if (word.length() > set.length()) {
+            return false;
+        }
+        List<String> listSet = new ArrayList<>();
+        for (var i = 0; i < set.length(); i++) {
+            String x = String.valueOf(set.charAt(i));
+            listSet.add(i, x);
+        }
+        for (var i = 0; i < lowerWord.length(); i++) {
+            String z = String.valueOf(lowerWord.charAt(i));
+            for (var j = 0; j < listSet.size(); j++) {
+                if (!listSet.contains(z)) {
+                    return false;
+                } else {
+                    int index = listSet.indexOf(z);
+                    listSet.remove(index);
+                    break;
+                }
             }
         }
+        return true;
     }
-    return true;
 }
 //END
