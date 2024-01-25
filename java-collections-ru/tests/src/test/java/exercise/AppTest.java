96af51f0567ad2cd90cef19c19219fb078b58079
@hexlet/cli <support@hexlet.io>
2024-01-25 19:38:09 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -14,10 +14,6 @@ class AppTest {
             List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
             List<Integer> result1 = App.take(numbers1, 2);
             assertThat(result1).containsExactly(1, 2);
-
-            List<Integer> numbers2 = new ArrayList<>(Arrays.asList(7, 3, 10));
-            List<Integer> result2 = App.take(numbers2, 8);
-            assertThat(result2).containsExactly(7, 3, 10);
         // END
     }
 }
