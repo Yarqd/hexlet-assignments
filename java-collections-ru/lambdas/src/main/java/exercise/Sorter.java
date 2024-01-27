cfc89f3c8ceb9dd0dbfec3a73e77e55e89b9f0fc
@hexlet/cli <support@hexlet.io>
2024-01-27 19:59:09 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+
+// BEGIN
+public class Sorter {
+    public static List<String> takeOldestMans(List<Map<String, String>> users) {
+        List<String> oldestMans = new ArrayList<>();
+        users.stream()
+                .filter(person -> "male".equals(person.get("gender")))
+                .sorted(Comparator.comparing(person -> LocalDate.parse(person.get("birthday"))))
+                .forEach(user -> oldestMans.add(user.get("name")));
+        return oldestMans;
+    }
+}
+// END
---
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -0,0 +1,30 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+// BEGIN
+@Test
+public void testNlargeArrayImage() {
+    String[][] originalImage = {
+            {"*", "*", "*", "*"},
+            {"*", " ", " ", "*"},
+            {"*", " ", " ", "*"},
+            {"*", "*", "*", "*"}
+    };
+    String[][] expectedEnlargedImage = {
+            {"*", "*", "*", "*", "*", "*", "*", "*"},
+            {"*", "*", "*", "*", "*", "*", "*", "*"},
+            {"*", " ", " ", " ", " ", " ", " ", "*"},
+            {"*", " ", " ", " ", " ", " ", " ", "*"},
+            {"*", " ", " ", " ", " ", " ", " ", "*"},
+            {"*", " ", " ", " ", " ", " ", " ", "*"},
+            {"*", "*", "*", "*", "*", "*", "*", "*"},
+            {"*", "*", "*", "*", "*", "*", "*", "*"}
+    };
+    String[][] actualEnlargedImage = ImageProcessor.nlargeArrayImage(originalImage);
+
+    assertThat(actualEnlargedImage).isEqualTo(expectedEnlargedImage);
+}
+}
+// END
