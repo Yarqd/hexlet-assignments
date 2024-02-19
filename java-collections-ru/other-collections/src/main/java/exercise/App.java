454ac906a7ee16d89444a7cdda81e57a5a21a696
@hexlet/cli <support@hexlet.io>
2024-02-19 16:04:00 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -9,20 +9,25 @@ import java.util.TreeSet;
 public class App {
     public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
         Map<String, String> result = new LinkedHashMap<>(); // Результирующий словарь
-        for (String key : data1.keySet()) { // Перебираем ключи из первого словаря
-            if (data2.containsKey(key)) { // Если ключ присутствует во втором словаре
-                if (data1.get(key).equals(data2.get(key))) { // И значения ключей равны
-                    result.put(key, "unchanged"); // Добавляем ключ со значением "unchanged" в результирующий словарь
+        // Перебираем ключи из первого словаря
+        for (String key : data1.keySet()) {
+            // Если ключ присутствует во втором словаре
+            if (data2.containsKey(key)) {
+                // Проверяем равенство значений
+                if (data1.get(key).equals(data2.get(key))) {
+                    result.put(key, "unchanged"); // Значения равны
                 } else {
-                    result.put(key, "changed"); // Иначе добавляем ключ со значением "changed"
+                    result.put(key, "changed"); // Значения отличаются
                 }
-            } else { // Если ключ отсутствует во втором словаре
-                result.put(key, "deleted"); // Добавляем ключ со значением "deleted"
+            } else {
+                result.put(key, "deleted"); // Ключ отсутствует во втором словаре
             }
         }
-        for (String key : data2.keySet()) { // Перебираем ключи из второго словаря
-            if (!data1.containsKey(key)) { // Если ключ отсутствует в первом словаре
-                result.put(key, "added"); // Добавляем ключ со значением "added" в результирующий словарь
+        // Перебираем ключи из второго словаря
+        for (String key : data2.keySet()) {
+            // Если ключ отсутствует в первом словаре
+            if (!data1.containsKey(key)) {
+                result.put(key, "added"); // Ключ добавлен
             }
         }
         return result;
