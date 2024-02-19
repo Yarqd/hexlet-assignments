e3bd42873b64eaacdbd91a1da0c615e5f8722d0b
@hexlet/cli <support@hexlet.io>
2024-02-19 16:25:26 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -1,7 +1,6 @@
 package exercise;
 
 import java.util.LinkedHashMap;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -9,23 +8,29 @@ import java.util.TreeSet;
 // BEGIN
 public class App {
     public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
-        Set<String> keys = new TreeSet<>(data1.keySet()); // Создаем TreeSet для автоматической сортировки ключей
-        keys.addAll(data2.keySet()); // Добавляем все ключи из обоих множеств
+        // Создаем TreeSet для автоматической сортировки ключей в алфавитном порядке
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
 
         Map<String, String> result = new LinkedHashMap<>(); // Результирующий словарь
+
         for (String key : keys) {
-            if (data1.containsKey(key) && data2.containsKey(key)) {
-                if (data1.get(key).equals(data2.get(key))) {
+            Object value1 = data1.get(key);
+            Object value2 = data2.get(key);
+
+            if (value1 != null && value2 != null) {
+                if (value1.equals(value2)) {
                     result.put(key, "unchanged");
                 } else {
                     result.put(key, "changed");
                 }
-            } else if (data1.containsKey(key)) {
+            } else if (value1 != null) {
                 result.put(key, "deleted");
             } else {
                 result.put(key, "added");
             }
         }
+
         return result;
     }
 //END
