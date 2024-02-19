5f1f22b2255e5cceb3edd66bbd359119eea6c777
@hexlet/cli <support@hexlet.io>
2024-02-19 15:37:31 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -25,19 +25,5 @@ public class App {
                 })
                 .collect(Collectors.joining(",")); // Собираем переменные в строку, разделяя запятыми
     }
-
-    public static void main(String[] args) {
-        String content = "[program:prepare]\n" +
-                "command=sudo -HEu tirion /bin/bash -c 'cd /usr/src/app && make prepare'\n" +
-                "autorestart=false\n" +
-                "environment=\"X_FORWARDED_MAIL=tirion@google.com,X_FORWARDED_HOME=/home/tirion,language=en\"\n" +
-                "\n" +
-                "[program:http_server]\n" +
-                "command=sudo -HEu tirion /bin/bash -c 'cd /usr/src/app && make environment'\n" +
-                "environment=\"key5=value5,X_FORWARDED_var3=value,key6=value6\"";
-
-        String result = App.getForwardedVariables(content);
-        System.out.println(result); // => "MAIL=tirion@google.com,HOME=/home/tirion,var3=value"
-    }
 }
 //END
