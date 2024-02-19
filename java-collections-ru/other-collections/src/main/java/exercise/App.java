654897fddb6646e96256932a1909f2aa3612718a
@hexlet/cli <support@hexlet.io>
2024-02-19 16:10:37 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -1,6 +1,7 @@
 package exercise;
 
 import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -9,25 +10,21 @@ import java.util.TreeSet;
 public class App {
     public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
         Map<String, String> result = new LinkedHashMap<>(); // Результирующий словарь
-        // Перебираем ключи из первого словаря
+        // Получаем объединение ключей из обоих словарей
         for (String key : data1.keySet()) {
-            // Если ключ присутствует во втором словаре
             if (data2.containsKey(key)) {
-                // Проверяем равенство значений
                 if (data1.get(key).equals(data2.get(key))) {
-                    result.put(key, "unchanged"); // Значения равны
+                    result.put(key, "unchanged");
                 } else {
-                    result.put(key, "changed"); // Значения отличаются
+                    result.put(key, "changed");
                 }
             } else {
-                result.put(key, "deleted"); // Ключ отсутствует во втором словаре
+                result.put(key, "deleted");
             }
         }
-        // Перебираем ключи из второго словаря
         for (String key : data2.keySet()) {
-            // Если ключ отсутствует в первом словаре
             if (!data1.containsKey(key)) {
-                result.put(key, "added"); // Ключ добавлен
+                result.put(key, "added");
             }
         }
         return result;
