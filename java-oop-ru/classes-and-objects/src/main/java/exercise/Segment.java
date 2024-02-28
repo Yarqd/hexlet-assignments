c74ef7c163272b6ae3afdb46ef64ca535154da76
@hexlet/cli <support@hexlet.io>
2024-02-28 15:24:49 +0300

    submit java-oop-ru/classes-and-objects

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -5,7 +5,7 @@ public class Segment {
     private Point one;
     private Point two;
 
-    public Segment (Point one, Point two) {
+    public Segment(Point one, Point two) {
         this.one = one;
         this.two = two;
     }
@@ -13,19 +13,19 @@ public class Segment {
     public int getBeginPoint() {
         int a = one.getX();
         int b = two.getX();
-        return (a+b)/2;
+        return (a + b) / 2;
     }
 
     public int getEndPoint() {
         int a = one.getY();
         int b = two.getY();
-        return (a+b)/2;
+        return (a + b) / 2;
     }
 
-    public Point getMidPoint() {
+    public int getMidPoint() {
         int a = getBeginPoint();
         int b = getEndPoint();
-        return (a+b)/2;
+        return (a + b) / 2;
     }
 }
 // END
