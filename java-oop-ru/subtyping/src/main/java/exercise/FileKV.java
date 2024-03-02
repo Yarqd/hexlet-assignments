994b3c53a6519b70abbfc748dcbaac4a8579dc54
@hexlet/cli <support@hexlet.io>
2024-03-02 14:38:06 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.IOException;
 
 public class FileKV implements KeyValueStorage {
     private final Path filePath;
