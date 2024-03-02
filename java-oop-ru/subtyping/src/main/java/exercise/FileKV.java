4b7d346f09241b088ca2d87f7d453f023679697d
@hexlet/cli <support@hexlet.io>
2024-03-02 15:18:57 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -44,14 +44,14 @@ public class FileKV implements KeyValueStorage {
         } catch (IOException e) {
             System.err.println("Failed to save data to file: " + e.getMessage());
         }
+    }
 
-        private void loadFromFile () {
-            try {
-                String content = Files.readString(filePath);
-                data = Utils.unserialize(content);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private void loadFromFile () {
+        try {
+            String content = Files.readString(filePath);
+            data = Utils.unserialize(content);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
