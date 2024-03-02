0853d3d5bd43bd6c2892f39d93a6e8dcc112b916
@hexlet/cli <support@hexlet.io>
2024-03-02 16:45:38 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -1,52 +1,53 @@
 package exercise;
 
 // BEGIN
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
-import java.util.HashMap;
-
 
 public class FileKV implements KeyValueStorage {
-    private Map<String, String> data = new HashMap<>();
     private final Path filepath;
-    private final Map<String, String> data;
+    private final Map<String, String> dataMap;
 
     public FileKV(Path filepath) {
         this.filepath = filepath;
-        this.data = new HashMap<>();
-    }
-
-    @Override
-    public void set(String key, String value) {
-        data.put(key, value);
-    }
-
-    @Override
-    public void unset(String key) {
-        data.remove(key);
+        this.dataMap = new HashMap<>();
     }
 
     @Override
     public String get(String key, String defaultValue) {
-        return data.getOrDefault(key, defaultValue);
+        return dataMap.getOrDefault(key, defaultValue);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        dataMap.put(key, value);
+    }
+
+    @Override
+    public void unset(String key) {
+        dataMap.remove(key);
     }
 
     @Override
     public Map<String, String> toMap() {
-        return data;
+        return new HashMap<>(dataMap);
     }
 
     @Override
     public void save() throws IOException {
-        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-            for (Map.Entry<String, String> entry : data.entrySet()) {
+        try (BufferedWriter writer = Files.newBufferedWriter(filepath)) {
+            for (Map.Entry<String, String> entry : dataMap.entrySet()) {
                 writer.write(entry.getKey() + "=" + entry.getValue());
                 writer.newLine();
             }
         }
+    }
+
+    @Override
+    public void clear() {
+        dataMap.clear();
     }
 }
 
