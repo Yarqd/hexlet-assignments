dff81d20f1774b06f6e1284a9b5958d8ceea3c68
@hexlet/cli <support@hexlet.io>
2024-03-02 16:15:04 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -2,7 +2,6 @@ package exercise;
 
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.HashMap;
 
 
--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
@@ -20,50 +21,27 @@ class FileKVTest {
     private static Path filepath = Paths.get("src/test/resources/file").toAbsolutePath().normalize();
 
     @BeforeEach
-    public void beforeEach() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        String content = mapper.writeValueAsString(new HashMap<String, String>());
-        Files.writeString(filepath, content, StandardOpenOption.CREATE);
+    public void beforeEach() throws IOException {
+        Files.deleteIfExists(filepath); // Удаляем файл перед каждым запуском теста
     }
 
     // BEGIN
-    private static final String TEST_FILE_PATH = "src/test/resources/file";
-
-    @BeforeEach
-    public void beforeEach() throws Exception {
-        Files.deleteIfExists(Paths.get(TEST_FILE_PATH));
-    }
-
     @Test
-    void testSetAndGet() {
-        FileKV storage = new FileKV(TEST_FILE_PATH, new HashMap<>());
-        storage.set("key", "value");
-        assertEquals("value", storage.get("key", null));
-    }
+    public void testFileKV() throws IOException {
+        // Arrange
+        Map<String, String> testData = new HashMap<>();
+        testData.put("key1", "value1");
+        testData.put("key2", "value2");
 
-    @Test
-    void testUnset() {
-        FileKV storage = new FileKV(TEST_FILE_PATH, new HashMap<>());
-        storage.set("key", "value");
-        storage.unset("key");
-        assertNull(storage.get("key", null));
-    }
+        // Act
+        String serializedData = Utils.serialize(testData);
+        Utils.writeFile(filepath.toString(), serializedData);
 
-    @Test
-    void testGetDefaultValue() {
-        FileKV storage = new FileKV(TEST_FILE_PATH, new HashMap<>());
-        assertNull(storage.get("key", null));
-        assertEquals("default", storage.get("key", "default"));
-    }
+        String fileContent = Utils.readFile(filepath.toString());
+        Map<String, String> deserializedData = Utils.unserialize(fileContent);
 
-    @Test
-    void testToMap() {
-        Map<String, String> initialData = new HashMap<>();
-        initialData.put("key1", "value1");
-        initialData.put("key2", "value2");
-        FileKV storage = new FileKV(TEST_FILE_PATH, initialData);
-        Map<String, String> data = storage.toMap();
-        assertEquals(initialData, data);
+        // Assert
+        assertEquals(testData, deserializedData);
     }
     // END
 }
