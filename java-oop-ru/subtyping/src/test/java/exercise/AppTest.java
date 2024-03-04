f6e11381573574e5d352e20ed61601318d1bfb48
@hexlet/cli <support@hexlet.io>
2024-03-04 13:41:07 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/test/java/exercise/AppTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/AppTest.java
@@ -22,6 +22,8 @@ class AppTest {
         KeyValueStorage storage = new InMemoryKV(Map.of("foo", "bar", "bar", "zoo"));
         App.swapKeyValue(storage);
         Map<String, String> expected = Map.of("bar", "foo", "zoo", "bar");
+        // Меняем местами ключи и значения, так как ожидается именно обмен
         assertThat(storage.toMap()).isEqualTo(expected);
     }
 }
+
