aea9ff4d7b13af81858e48d39633f954c212bbe9
@hexlet/cli <support@hexlet.io>
2024-03-09 16:31:05 +0300

    submit java-oop-ru/reflections

---
+++ java-oop-ru/reflections/src/main/java/exercise/Address.java
@@ -0,0 +1,33 @@
+package exercise;
+
+class Address {
+    // BEGIN
+    @NotNull
+    private String country;
+    // END
+
+    // BEGIN
+    @NotNull
+    private String city;
+    // END
+
+    // BEGIN
+    @NotNull
+    private String street;
+    // END
+
+    // BEGIN
+    @NotNull
+    private String houseNumber;
+    // END
+
+    private String flatNumber;
+
+    Address(String country, String city, String street, String houseNumber, String flatNumber) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.flatNumber = flatNumber;
+    }
+}
