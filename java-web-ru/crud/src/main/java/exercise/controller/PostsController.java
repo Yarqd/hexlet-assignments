b4f1795797af5e210c3978c7539a2bd0210ec85c
@hexlet/cli <support@hexlet.io>
2024-05-10 15:07:46 +0300

    submit java-web-ru/crud

--- java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -9,7 +9,13 @@ import exercise.dto.posts.PostsPage;
 public class PostsController {
 
     public static void listPosts(Context ctx) {
-        int page = Integer.parseInt(ctx.queryParam("page", "1"));
+        int page;
+        try {
+            page = Integer.parseInt(ctx.queryParam("page", "1"));
+        } catch (NumberFormatException e) {
+            page = 1;
+        }
+
         int pageSize = 5;
         var posts = PostRepository.findAll(page, pageSize);
         int totalPages = (int) Math.ceil((double) PostRepository.getEntities().size() / pageSize);
@@ -19,7 +25,13 @@ public class PostsController {
     }
 
     public static void showPost(Context ctx) {
-        Long id = Long.parseLong(ctx.pathParam("id"));
+        Long id;
+        try {
+            id = Long.parseLong(ctx.pathParam("id"));
+        } catch (NumberFormatException e) {
+            throw new NotFoundResponse("Invalid ID format");
+        }
+
         var post = PostRepository.find(id).orElseThrow(() ->
                 new NotFoundResponse("Entity with id = " + id + " not found"));
         ctx.render("posts/show.jte", model("post", post));
