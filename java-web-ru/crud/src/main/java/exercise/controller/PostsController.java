d59d18ee1aed1287c90b00f9fab1736df9ec843e
@hexlet/cli <support@hexlet.io>
2024-05-10 14:15:40 +0300

    submit java-web-ru/crud

--- java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -6,20 +6,20 @@ import java.util.List;
 import exercise.model.Post;
 import exercise.repository.PostRepository;
 import exercise.dto.posts.PostsPage;
-import exercise.util.NamedRoutes;
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class PostsController {
 
-    public static void index(Context ctx) {
-        int page = ctx.queryParam("page", Integer.class).getOrDefault(1);
-        int size = 5;
+    public static void listPosts(Context ctx) {
+        int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
+        int size = 5; // Размер страницы пагинации
         List<Post> posts = PostRepository.findAll(page, size);
         var postsPage = new PostsPage(posts, page, PostRepository.count());
         ctx.render("posts/index.jte", model("page", postsPage));
     }
 
-    public static void show(Context ctx) {
-        Long id = ctx.pathParam("id", Long.class).get();
+    public static void showPost(Context ctx) {
+        Long id = ctx.pathParamAsClass("id", Long.class).get();
         Post post = PostRepository.find(id).orElseThrow(() -> new NotFoundResponse("Page not found"));
         ctx.render("posts/show.jte", model("post", post));
     }
