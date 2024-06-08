c69ebc38fd3df11c7c02f20572f600683aed3cae
@hexlet/cli <support@hexlet.io>
2024-06-08 11:31:28 +0300

    submit java-spring-ru/jpa-data

--- java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.Optional;
 
 import java.util.List;
 
--- java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -7,16 +7,18 @@ import jakarta.persistence.Table;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.*;
 
 // BEGIN
 @Entity
 @Table(name = "people")
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Person {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String firstName;
