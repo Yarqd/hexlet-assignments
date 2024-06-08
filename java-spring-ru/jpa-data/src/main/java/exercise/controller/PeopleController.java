aab7063658ef2ef44c43b9c225725597ce9b31ee
@hexlet/cli <support@hexlet.io>
2024-06-08 11:27:15 +0300

    submit java-spring-ru/jpa-data

---
+++ java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,56 @@
+package exercise.controller;
+
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+import exercise.model.Person;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person show(@PathVariable long id) {
+        return personRepository.findById(id).get();
+    }
+
+    // BEGIN
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personRepository.findAll();
+    }
+
+    // POST /people – создание новой персоны
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Person createPerson(@RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    // DELETE /people/{id} – удаление персоны
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePerson(@PathVariable Long id) {
+        Optional<Person> person = personRepository.findById(id);
+        if (person.isPresent()) {
+            personRepository.delete(person.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    // END
+}
