db45dde560a287daa432b88395ce3af547428716
@hexlet/cli <support@hexlet.io>
2024-06-16 00:50:51 +0300

    submit java-spring-ru/partial-update

--- java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
+++ java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
@@ -12,7 +12,7 @@ import exercise.dto.CarDTO;
 import exercise.model.Car;
 import org.mapstruct.MappingTarget;
 
-// BEGIN
+//BEGIN
 @Mapper(
         uses = { JsonNullableMapper.class },
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
@@ -23,10 +23,33 @@ public abstract class CarMapper {
 
     public abstract void update(CarUpdateDTO dto, @MappingTarget Car car);
 
-    @Mapping(target = "id", ignore = true) // Assuming id is managed elsewhere
+    @Mapping(target = "id", ignore = true)
     public abstract CarDTO mapToDTO(Car car);
 
-    @Mapping(target = "id", ignore = true) // Assuming id is managed elsewhere
+    @Mapping(target = "id", ignore = true)
     public abstract Car mapToEntity(CarCreateDTO dto);
+
+    public CarDTO mapToDTO(Car car) {
+        if (car == null) {
+            return null;
+        }
+        CarDTO carDTO = new CarDTO();
+        carDTO.setId(car.getId());
+        carDTO.setModel(car.getModel());
+        carDTO.setManufacturer(car.getManufacturer());
+        carDTO.setEnginePower(car.getEnginePower());
+        return carDTO;
+    }
+
+    public Car mapToEntity(CarCreateDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+        Car car = new Car();
+        car.setModel(dto.getModel());
+        car.setManufacturer(dto.getManufacturer());
+        car.setEnginePower(dto.getEnginePower());
+        return car;
+    }
 }
 // END
