1cf77d3cce53c719d860fefb9f70868188db4ae3
@hexlet/cli <support@hexlet.io>
2024-06-17 18:05:33 +0300

    submit java-spring-ru/partial-update

--- java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
+++ java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
@@ -5,31 +5,41 @@ import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-import org.openapitools.jackson.nullable.JsonNullable;
 
 import exercise.dto.CarCreateDTO;
 import exercise.dto.CarUpdateDTO;
 import exercise.dto.CarDTO;
 import exercise.model.Car;
+import org.mapstruct.Context;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 // BEGIN
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = JsonNullableMapper.class,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface CarMapper {
     CarDTO toCarDTO(Car car);
+
     Car toCar(CarCreateDTO carCreateDTO);
 
-    default void updateCarFromDto(CarUpdateDTO dto, @MappingTarget Car car) {
-        if (dto.getModel() != null && dto.getModel().isPresent()) {
-            car.setModel(dto.getModel().get());
-        }
-        if (dto.getManufacturer() != null && dto.getManufacturer().isPresent()) {
-            car.setManufacturer(dto.getManufacturer().get());
-        }
-        if (dto.getEnginePower() != null && dto.getEnginePower().isPresent()) {
-            car.setEnginePower(dto.getEnginePower().get());
-        }
+    void updateCarFromDto(CarUpdateDTO carUpdateDTO, @MappingTarget Car car, @Context JsonNullableMapper jsonNullableMapper);
+}
+
+package exercise.mapper;
+
+import org.openapitools.jackson.nullable.JsonNullable;
+
+public class JsonNullableMapper {
+
+    public <T> T unwrap(JsonNullable<T> jsonNullable) {
+        return jsonNullable == null ? null : jsonNullable.orElse(null);
+    }
+
+    public <T> boolean isPresent(JsonNullable<T> jsonNullable) {
+        return jsonNullable != null && jsonNullable.isPresent();
     }
 }
 // END
