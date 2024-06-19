5abdceb711d5d2d091366577524ea220b82340a4
@hexlet/cli <support@hexlet.io>
2024-06-19 17:08:42 +0300

    submit java-spring-ru/crud

--- java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import exercise.exception.ResourceNotFoundException;
 import jakarta.validation.Valid;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 @RestController
 @RequestMapping("/products")
