5c32e3411f9deecdf392f5e956574c4aa17781a3
@hexlet/cli <support@hexlet.io>
2024-06-25 15:08:25 +0300

    submit java-web-ru/template

--- java-web-ru/template/src/main/java/exercise/App.java
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -22,7 +22,7 @@ public final class App {
 
         // BEGIN
         app.get("/users", ctx -> {
-            var page = new UserPage(USERS);
+            var page = new UsersPage(USERS);
             ctx.render("users/index.jte", model("page", page));
         });
 
