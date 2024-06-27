afbf148313b10eb3def777a643e27baa1073b943
@hexlet/cli <support@hexlet.io>
2024-06-27 18:16:45 +0300

    submit java-web-ru/get-form

--- java-web-ru/get-form/src/main/java/exercise/App.java
+++ java-web-ru/get-form/src/main/java/exercise/App.java
@@ -24,25 +24,16 @@ public final class App {
         // BEGIN
         app.get("/users", ctx -> {
             var term = ctx.queryParam("term");
-            List<User> users;
+            List<User> users = new ArrayList<>(USERS);
+
             if (term != null && !term.isEmpty()) {
-                users = filterUsersByTerm(term);
-            } else {
-                users = USERS;
+                term = term.toLowerCase();
+                users.removeIf(user -> !user.getFirstName().toLowerCase().startsWith(term));
             }
+
             var page = new UsersPage(users, term);
             ctx.render("users/index.jte", model("page", page));
         });
-
-        private static List<User> filterUsersByTerm(String term) {
-            List<User> filteredUsers = new ArrayList<>();
-            for (User user : USERS) {
-                if (user.getFirstName().toLowerCase().startsWith(term.toLowerCase())) {
-                    filteredUsers.add(user);
-                }
-            }
-            return filteredUsers;
-        }
         // END
 
         app.get("/", ctx -> {
