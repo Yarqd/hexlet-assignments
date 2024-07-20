0987a16a2d753c1b6a5d76eede8d796b77d1577b
@hexlet/cli <support@hexlet.io>
2024-07-20 17:50:41 +0300

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,48 +1,37 @@
 package exercise.controller.users;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 
 import exercise.model.Post;
 import exercise.Data;
 
+// BEGIN
 @RestController
-@RequestMapping("/api/users/{userId}/posts")
+@RequestMapping("/api/users/{id}/posts")
 public class PostsController {
-
-    // BEGIN
     private List<Post> posts = Data.getPosts();
 
     @GetMapping
-    public ResponseEntity<List<Post>> getUserPosts(@PathVariable int userId) {
-        // Фильтруем посты по userId и собираем их в список
+    public ResponseEntity<List<Post>> getUserPosts(@PathVariable int id) {
         List<Post> userPosts = posts.stream()
-                .filter(post -> post.getUserId() == userId)
+                .filter(post -> post.getUserId() == id)
                 .collect(Collectors.toList());
-
-        // Возвращаем статус 200 OK и список постов
         return ResponseEntity.ok(userPosts);
     }
 
     @PostMapping
-    public ResponseEntity<Post> createPost(@PathVariable int userId, @RequestBody Post newPost) {
-        // Устанавливаем userId из маршрута
-        newPost.setUserId(userId);
-
-        // Добавляем новый пост в список постов
-        posts.add(newPost);
-
-        // Возвращаем статус 201 Created и сам пост
-        return ResponseEntity.status(HttpStatus.CREATED).body(newPost);
+    public ResponseEntity<Post> createUserPost(@PathVariable int id, @RequestBody Post post) {
+        post.setUserId(id);
+        posts.add(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(post);
     }
-    // END
 }
+// END
