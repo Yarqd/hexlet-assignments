8bad1dc4240cc6b682bd549c683e9c5b9c5cae23
@hexlet/cli <support@hexlet.io>
2024-08-11 16:04:48 +0300

    submit java-spring-ru/crud

--- java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
@@ -1,26 +1,20 @@
 package exercise.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import exercise.dto.ProductCreateDTO;
 import exercise.dto.ProductDTO;
 import exercise.dto.ProductUpdateDTO;
 import exercise.mapper.ProductMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import exercise.exception.ResourceNotFoundException;
+import exercise.model.Category;
+import exercise.repository.CategoryRepository;
 import exercise.repository.ProductRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -29,9 +23,11 @@ public class ProductsController {
     private ProductRepository productRepository;
 
     @Autowired
+    private CategoryRepository categoryRepository; // Добавляем CategoryRepository
+
+    @Autowired
     private ProductMapper productMapper;
 
-    // BEGIN
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductDTO> index() {
@@ -88,5 +84,4 @@ public class ProductsController {
         }
         productRepository.deleteById(id);
     }
-    // END
 }
